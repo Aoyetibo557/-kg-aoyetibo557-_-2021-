360c177e812eff9a78fe4f0c06afebb506aa975f
Anuoluwapo Oyetibo <Aoyetibo@gmail.com>
2020-03-13 21:43:36 -0400

    //Final Solution in. Takes in two input as string values. //First checks the length of both string to make sure they match else terminate/ return false.

---
+++ src/main.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class main {
+
+	static int size = 500;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a="aac";
+		String b="bbs";
+		
+		System.out.println(one_to_one(a,b));
+
+	}
+	
+	public static boolean one_to_one(String s1,String s2) {
+		
+		int s1Length=s1.length();
+		int s2Length=s2.length();
+		
+		if(s1Length != s2Length)
+			return false;
+					
+		Boolean[] isMarked = new Boolean[size];
+		Arrays.fill(isMarked, Boolean.FALSE);	//default false
+		
+		//need a map
+		int[] map =new int[size];
+		Arrays.fill(map, -1);
+		
+		for(int i=0;i<s1Length;i++) {
+			if(map[s1.charAt(i)] == -1) {
+				if(isMarked[s2.charAt(i)] == true)
+					return false;
+				
+					//mark as seen/parsed
+					isMarked[s2.charAt(i)] =true;
+					
+					map[s1.charAt(i)] =s2.charAt(i);	//to store the mapping of the current value 
+				}
+				else if(map[s1.charAt(i)] != s2.charAt(i)) {
+					return false;
+				}
+		}
+		return true;
+	}
+}
